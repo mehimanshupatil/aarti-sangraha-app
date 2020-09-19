951c603529635011fb871154367a5029599c54ed
HP <himanshu.patil@bizotics.com>
2020-09-19 09:59:52 +0530

    splash icons added

--- screens/about.tsx
+++ screens/about.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, Text, TouchableOpacity, AsyncStorage } from 'react-native';
 import * as Linking from 'expo-linking';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default function About() {
 
@@ -8,21 +9,23 @@ export default function About() {
         Linking.openURL(url);
     }
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Thanks for using app. Please rate on Play Store{"\n"}{"\n"}
+        <ScrollView>
+            <View style={styles.container}>
+                <Text style={styles.text}>Thanks for using app. Please rate on Play Store{"\n"}{"\n"}
                  Source code of app is available at
                 <Text style={styles.texturl} onPress={() => handlePress('https://github.com/mehimanshupatil/aarti-sangraha-app')}> github</Text>
 
-                {"\n"}{"\n"}
+                    {"\n"}{"\n"}
                    For suggestion mail at{"\n"}
-                <Text style={styles.texturl} onPress={() => handlePress('mailto:mailhimanshupatil@gmail.com')}>mailhimanshupatil@gmail.com</Text>
-                {"\n"}{"\n"}<Text>Credits:</Text>{"\n"}
+                    <Text style={styles.texturl} onPress={() => handlePress('mailto:mailhimanshupatil@gmail.com')}>mailhimanshupatil@gmail.com</Text>
+                    {"\n"}{"\n"}<Text>Credits:</Text>{"\n"}
             Application Icon and Splash Icon Image by
           <Text style={styles.texturl} onPress={() => handlePress('https://pixabay.com/users/AdventureTravelTrip-7440487/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=4341353')}> Hitesh Sharma </Text>
                 from
           <Text style={styles.texturl} onPress={() => handlePress('https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=4341353')}> Pixabay </Text>
-            </Text>
-        </View>
+                </Text>
+            </View>
+        </ScrollView>
     );
 
 
